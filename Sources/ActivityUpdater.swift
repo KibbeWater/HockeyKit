8de765c5b5d79bdc203fb65373aef05f9b6628ff
KibbeWater <35224538+KibbeWater@users.noreply.github.com>
2024-03-27 13:48:12 +0100

    Added activity start request

--- Sources/ActivityUpdater.swift
+++ Sources/ActivityUpdater.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import ActivityKit
 
 public class ActivityUpdater {
     public static var shared: ActivityUpdater = ActivityUpdater()
@@ -14,7 +15,28 @@ public class ActivityUpdater {
         return SHLWidgetAttributes.ContentState(homeScore: overview.homeGoals, awayScore: overview.awayGoals, period: ActivityPeriod(period: overview.time.period, periodEnd: overview.time.periodEnd ?? Date()))
     }
     
-    public static func start(match: GameOverview) throws {
+    func OverviewToAttrib(_ overview: GameOverview) -> SHLWidgetAttributes {
+        return SHLWidgetAttributes(homeTeam: ActivityTeam(name: overview.homeTeam.teamName, teamCode: overview.homeTeam.teamCode), awayTeam: ActivityTeam(name: overview.awayTeam.teamName, teamCode: overview.awayTeam.teamCode))
+    }
+    
+    public func start(match: GameOverview) throws {
+        let attrib = OverviewToAttrib(match)
+        let initState = OverviewToState(match)
         
+        let activity = try Activity.request(
+            attributes: attrib,
+            content: .init(state: initState, staleDate: nil),
+            pushType: .token
+        )
+        
+        Task {
+            for await pushToken in activity.pushTokenUpdates {
+                let pushTokenString = pushToken.reduce("") {
+                    $0 + String(format: "%02x", $1)
+                }
+                
+                // Send the push token
+            }
+        }
     }
 }
