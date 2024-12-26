273929fddb35802a37fdfc22fe1f14040910f13b
KibbeWater <35224538+KibbeWater@users.noreply.github.com>
2024-12-26 16:46:19 +0100

    chore(match): add disclaimer to getMatch

--- Sources/HockeyKit/Protocols/MatchServiceProtocol.swift
+++ Sources/HockeyKit/Protocols/MatchServiceProtocol.swift
@@ -13,7 +13,18 @@ public protocol MatchServiceProtocol {
     func getSeasonSchedule(_ season: Season) async throws -> [Game]
     func getSeasonSchedule(_ season: Season, withTeams: [String]) async throws -> [Game]
     
+    /// Retreive game information about live games
+    ///
+    /// > Warning: This function is unable to check if a game is live or not,
+    /// > checks needs to be done by the developer where needed to ensure
+    /// > the function won't error
+    ///
+    /// - Parameters:
+    ///     - matchId: A live or already played game.
+    ///
+    /// - Returns: Information about a live game.
     func getMatch(_ matchId: String) async throws -> GameData
+    
     func getMatchStats(_ game: Game) async throws -> GameStats
     func getMatchExtra(_ game: Game) async throws -> GameExtra
     func getMatchPBP(_ game: Game) async throws -> PBPEvents
