6dc4e9d969091e7530ec11b7d2e9f7ee1799d058
KibbeWater <35224538+KibbeWater@users.noreply.github.com>
2025-01-09 16:36:42 +0100

    test(date): additional tests

--- Tests/HockeyKitTests/Utils/DateTests.swift
+++ Tests/HockeyKitTests/Utils/DateTests.swift
@@ -15,7 +15,8 @@ import Foundation
         ("2024-11-28T18:00:00.000Z", Date(timeIntervalSince1970: 1732820400)),
         ("2024-09-21T13:15:00+0200", Date(timeIntervalSince1970: 1726917300)),
         ("2025-02-20T18:00:00+0100", Date(timeIntervalSince1970: 1740070800)),*/
-        ("2024-12-19T18:00:00.000Z", Date(timeIntervalSince1970: 1734631200))
+        ("2024-12-19T18:00:00.000Z", Date(timeIntervalSince1970: 1734631200)),
+        ("2025-01-09T18:00:00.000Z", Date(timeIntervalSince1970: 1736445600))
     ])
     func testISOToDate(date: String, expected: Date) async throws {
         #expect(expected == DateUtils.parseISODate(date))
