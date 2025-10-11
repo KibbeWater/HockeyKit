7ccaa0669cc1356d3104e602a22b785ca2e58970
KibbeWater <35224538+KibbeWater@users.noreply.github.com>
2025-10-11 19:03:43 +0200

    fix(api): make listenerservice lazyloaded
    This is in reference to an issue where if multiple instances of HockeyKit are being initialized, it will crash an application due to an issue within the ListenerService itself. To solve this, we only init it when needed

--- Sources/HockeyKit/HockeyAPI.swift
+++ Sources/HockeyKit/HockeyAPI.swift
@@ -8,23 +8,24 @@
 import Foundation
 
 public class HockeyAPI {
+    private let networkManager: NetworkManager
     public let season: SeasonServiceProtocol
     public let match: MatchServiceProtocol
     public let team: TeamServiceProtocol
     public let series: SeriesServiceProtocol
     public let standings: StandingServiceProtocol
-    public let listener: ListenerServiceProtocol
+    public lazy var listener: ListenerServiceProtocol = ListenerService()
     public let player: PlayerServiceProtocol
 
     public init() {
         let networkManager = NetworkManager()
-        
+        self.networkManager = networkManager
+
         self.season = SeasonService(networkManager: networkManager)
         self.match = MatchService(networkManager: networkManager)
         self.team = TeamService(networkManager: networkManager)
         self.series = SeriesService(networkManager: networkManager)
         self.standings = StandingService(networkManager: networkManager)
         self.player = PlayerService(networkManager: networkManager)
-        self.listener = ListenerService()
     }
 }
