8ed732a578e1422126a2c4bc844ada16ccb917f1
KibbeWater <35224538+KibbeWater@users.noreply.github.com>
2025-10-12 12:06:30 +0200

    fix(player): weight may not be available

--- Sources/HockeyKit/Models/Player.swift
+++ Sources/HockeyKit/Models/Player.swift
@@ -24,18 +24,18 @@ public struct Player: Codable, Equatable, Sendable {
     public let height: PlayerAttribute
     public let jerseyNumber: Int?
     public let seasonStats: [PlayerAttribute]?
-    public let weight: PlayerAttribute
+    public let weight: PlayerAttribute?
     public let team: PlayerTeam
-    
+
     public func getStats(for key: PlayerStatisticKey) -> Float? {
         let stats = seasonStats?.first(where: { $0.field == key.rawValue })
         return stats?.value
     }
-    
+
     public static func == (lhs: Player, rhs: Player) -> Bool {
         lhs.uuid == rhs.uuid
     }
-    
+
     public struct PlayerTeam: Codable, Sendable {
         public let name: String
         public let code: String
@@ -45,16 +45,16 @@ public struct Player: Codable, Equatable, Sendable {
         let value: Float
         let format: String?
         let field: String?
-        
+
         public init(from decoder: any Decoder) throws {
             let container = try decoder.container(keyedBy: CodingKeys.self)
-            
+
             let _floatVal = try? container.decodeIfPresent(Float.self, forKey: .value)
             let _stringVal = try? container.decodeIfPresent(String.self, forKey: .value)
-            
+
             let ss = _stringVal.map(Float.init)
             self.value = _floatVal ?? (ss ?? 0.0)!
-            
+
             self.format = try container.decodeIfPresent(String.self, forKey: .format)
             self.field = try container.decodeIfPresent(String.self, forKey: .field)
         }
