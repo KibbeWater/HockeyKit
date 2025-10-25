789fcc7e112597b5b4a1d616ba497a63298f6d4a
KibbeWater <35224538+KibbeWater@users.noreply.github.com>
2025-10-25 19:41:12 +0200

    fix: improved GameTime estimation

--- Sources/HockeyKit/Models/GameData.swift
+++ Sources/HockeyKit/Models/GameData.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public struct GameData: Codable, Sendable {
     public var gameOverview: GameOverview
-    
+
     public struct GameOverview: Codable, Equatable, Sendable {
         public var gameId: Int?
         public var homeTeam: TeamData
@@ -19,7 +19,7 @@ public struct GameData: Codable, Sendable {
         public var state: GameState
         public var gameUuid: String
         public var time: GameTime
-        
+
         public enum GameState: String, Codable, Sendable {
             case starting = "NotStarted"
             case ongoing = "Ongoing"
@@ -27,7 +27,7 @@ public struct GameData: Codable, Sendable {
             case overtime = "Overtime"
             case ended = "GameEnded"
         }
-        
+
         public struct TeamData: Codable, Equatable, Sendable, TeamTransformable {
             public var gameId: Int
             public var place: PlaceType
@@ -36,7 +36,7 @@ public struct GameData: Codable, Sendable {
             public var teamName: String
             public var teamCode: String
             public var gameUuid: String
-            
+
             public func toTeam() -> Team {
                 Team(
                     name: teamName,
@@ -44,62 +44,74 @@ public struct GameData: Codable, Sendable {
                     result: score
                 )
             }
-            
+
             public enum PlaceType: String, Codable, Sendable {
                 case home = "home"
                 case away = "away"
             }
         }
-        
+
         public struct GameTime: Codable, Equatable, Sendable {
             public var period: Int
             public var periodTime: String
             public var periodEnd: Date?
-            
+
             public func encode(to encoder: Encoder) throws {
                 var container = encoder.container(keyedBy: CodingKeys.self)
                 try container.encode(period, forKey: .period)
                 try container.encode(periodTime, forKey: .periodTime)
             }
-            
+
             public init(from decoder: Decoder) throws {
                 let container = try decoder.container(keyedBy: CodingKeys.self)
                 period = try container.decode(Int.self, forKey: .period)
                 periodTime = try container.decode(String.self, forKey: .periodTime)
                 periodEnd = estimatedEndTime(periodTime)
             }
-            
+
             public init(period: Int, periodTime: String, periodEnd: Date?) {
                 self.period = period
                 self.periodTime = periodTime
                 self.periodEnd = periodEnd
             }
-            
+
             // Coding keys for mapping between struct properties and JSON keys
             private enum CodingKeys: String, CodingKey {
                 case period
                 case periodTime
             }
-            
+
             public func estimatedEndTime(_ gameDuration: String) -> Date? {
-                let formatter = DateFormatter()
-                formatter.dateFormat = "mm:ss"
-                
-                guard let durationDate = formatter.date(from: gameDuration) else {
+                // Parse the game duration string (format: "mm:ss")
+                let components = gameDuration.split(separator: ":").compactMap { Int($0) }
+
+                guard components.count == 2 else {
                     return nil
                 }
-                
-                // Set the maximum game duration to 20 minutes
+
+                let minutes = components[0]
+                let seconds = components[1]
+
+                // Convert to total seconds elapsed
+                let elapsedSeconds = TimeInterval(minutes * 60 + seconds)
+
+                // Set the maximum game duration to 20 minutes (in seconds)
                 let maxGameDuration: TimeInterval = 20 * 60
-                
-                // Calculate the end time by adding the game duration to the current time
-                let estimatedEndDate = Date().addingTimeInterval(durationDate.timeIntervalSinceReferenceDate - maxGameDuration)
-                
+
+                // Calculate remaining time
+                let remainingSeconds = maxGameDuration - elapsedSeconds
+
+                // Calculate the estimated end time by adding remaining time to current time
+                let estimatedEndDate = Date().addingTimeInterval(remainingSeconds)
+
                 return estimatedEndDate
             }
         }
-        
-        public init(gameId: Int? = nil, homeTeam: TeamData, awayTeam: TeamData, homeGoals: Int, awayGoals: Int, state: GameState, gameUuid: String, time: GameTime) {
+
+        public init(
+            gameId: Int? = nil, homeTeam: TeamData, awayTeam: TeamData, homeGoals: Int,
+            awayGoals: Int, state: GameState, gameUuid: String, time: GameTime
+        ) {
             self.gameId = gameId
             self.homeTeam = homeTeam
             self.awayTeam = awayTeam
@@ -109,21 +121,26 @@ public struct GameData: Codable, Sendable {
             self.gameUuid = gameUuid
             self.time = time
         }
-        
+
         public init(from decoder: any Decoder) throws {
-            let container: KeyedDecodingContainer<GameData.GameOverview.CodingKeys> = try decoder.container(keyedBy: GameData.GameOverview.CodingKeys.self)
+            let container: KeyedDecodingContainer<GameData.GameOverview.CodingKeys> =
+                try decoder.container(keyedBy: GameData.GameOverview.CodingKeys.self)
 
             // Handle gameId which can be either Int or String from the API
             if let gameIdInt = try? container.decodeIfPresent(Int.self, forKey: .gameId) {
                 self.gameId = gameIdInt
-            } else if let gameIdString = try? container.decodeIfPresent(String.self, forKey: .gameId) {
+            } else if let gameIdString = try? container.decodeIfPresent(
+                String.self, forKey: .gameId)
+            {
                 self.gameId = Int(gameIdString)
             } else {
                 self.gameId = nil
             }
 
-            self.homeTeam = try container.decode(GameData.GameOverview.TeamData.self, forKey: .homeTeam)
-            self.awayTeam = try container.decode(GameData.GameOverview.TeamData.self, forKey: .awayTeam)
+            self.homeTeam = try container.decode(
+                GameData.GameOverview.TeamData.self, forKey: .homeTeam)
+            self.awayTeam = try container.decode(
+                GameData.GameOverview.TeamData.self, forKey: .awayTeam)
             self.homeGoals = try container.decode(Int.self, forKey: .homeGoals)
             self.awayGoals = try container.decode(Int.self, forKey: .awayGoals)
             self.state = try container.decode(GameData.GameOverview.GameState.self, forKey: .state)
@@ -155,4 +172,3 @@ public struct GameData: Codable, Sendable {
         }
     }
 }
-
